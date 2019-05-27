1b11026c5c08e7d6c17989bb24cb03a36b9c21c0
Sumit Tuladhar <sumittuladhar@gmail.com>
2019-05-27 09:37:02 +0545

    :smile: integrated dropzone and cloudinary

--- resources/js/components/MenuItem.vue
+++ resources/js/components/MenuItem.vue
@@ -24,6 +24,8 @@
       </select>
     </div>
 
+    <drop-zone :options="dropzoneOptions" id="dz" ref="dropzone"></drop-zone>
+
     <button class="btn btn-primary" type="submit">Save</button>
 
     <ul>
@@ -33,11 +35,36 @@
 </template>
 
 <script>
+  import vue2Dropzone from 'vue2-dropzone';
+  import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+
   export default {
+    components: {
+      dropZone: vue2Dropzone
+    },
     name: "MenuItem",
     props: ['initial-categories'],
     data() {
       return {
+        dropzoneOptions: {
+          // url: '/api/add-image',
+          url: 'https://api.cloudinary.com/v1_1/sumit270/image/upload',
+          thumbnailWidth: '200',
+          header: {
+            'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
+          },
+          sending(file, xhr, formData){
+            formData.append('api_key', 837962715986897);
+            formData.append('timestamp', Date.now() / 1000 | 0);
+            formData.append('upload_preset', 'vajraRestaurant');
+          },
+          success(file,res){
+            // console.log(res);
+            // console.log(file);
+            file.filename = res;
+            // console.log('Success! Cloudinary public ID is', res.public_id);
+          }
+        },
         item: {
           name: "",
           price: 0.00,
@@ -49,6 +76,12 @@
     },
     methods: {
       save() {
+        let files = this.$refs.dropzone.getAcceptedFiles();
+        // console.log(files);
+        // Grab cloudinary return filename.url
+        if(files.length > 0 && files[0].filename.url){
+          this.item.image = files[0].filename.url;
+        }
         axios.post('/api/menu-items/add', this.item)
           .then(res => {
             this.$router.push('/');
